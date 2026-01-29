3b5b5f6fab6694934ff465217f607974b985e191
LENOVO <anjanaheshan676@gmail.com>
2026-01-29 13:21:59 +0530

    User Checking before Loging bug fixed

--- app/index.tsx
+++ app/index.tsx
@@ -2,17 +2,28 @@ import { Image, StyleSheet, Text, View } from 'react-native'
 import React, { useEffect } from 'react'
 import '../global.css'
 import { colors } from '@/constants/theme'
-import { useRouter } from 'expo-router'
+import { Redirect, useRouter } from 'expo-router'
+import { useAuth } from '@/hooks/useAuth'
 
 const index = () => {
 
   const router = useRouter();
+  const { user, loading } = useAuth();
 
   useEffect(() => {
-    setTimeout(() => {
-      router.push('/welcome');
-    }, 2000);
-  }, []);
+    
+    if (!loading) {
+      const timer = setTimeout(() => {
+        if (user) {
+          router.replace('/(dashboard)/home');
+        } else {
+          router.replace('/welcome');
+        }
+      }, 2000);
+
+      return () => clearTimeout(timer);
+    }
+  }, [user, loading]);
 
   return (
     <View 
@@ -26,6 +37,12 @@ const index = () => {
       />
     </View>
   )
+
+    // if (user) {
+    //   return <Redirect href="/home" />
+    // }
+
+    // return <Redirect href="/login" />
 }
 
 export default index
